dec0defc2760f4a9263ed87486b30727058a0482
Matt Wylder <mwylder@paypal.com>
2022-07-28 13:48:40 -0500

    Initial Commit

---
+++ ImportHell/ImportHell.h
@@ -0,0 +1,18 @@
+//
+//  ImportHell.h
+//  ImportHell
+//
+//  Created by Matthew Wylder on 7/28/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImportHell.
+FOUNDATION_EXPORT double ImportHellVersionNumber;
+
+//! Project version string for ImportHell.
+FOUNDATION_EXPORT const unsigned char ImportHellVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImportHell/PublicHeader.h>
+
+
